6a19e8089f9d20e4811c55e5d6378b1da0ae3a0b
Tudor Anastasiu <tg.anastasiu@gmail.com>
2015-11-15 22:03:30 +0200

    liniar solution for Cf 596B

--- codeforces/596B/main.cpp
+++ codeforces/596B/main.cpp
@@ -7,39 +7,18 @@ typedef long long ll;
 
 const int MAXN = 200001;
 int n, a[MAXN];
-
-ll BIT[MAXN], ans;
-void update(ll x, ll val)
-{
-      for(; x <= n; x += x&-x)
-        BIT[x] += val;
-}
-ll query(ll x)
-{
-     ll sum = 0;
-     for(; x > 0; x -= x&-x)
-        sum += BIT[x];
-     return sum;
-}
-ll query_single(ll x) {
-    return query(n) - query(x);
-}
+ll ans;
 
 int main()
 {
-    //freopen("input", "r", stdin);
-
     cin >> n;
     for (int i = 1; i <= n; ++i) {
         cin >> a[i];
     }
 
-    for (int i = 1; i <= n; ++i) {
-        int val = a[i] - query_single(n - i + 1);
-        if (val != 0) {
-            ans += abs(val);
-            update(n - i + 1, val);
-        }
+    ans += abs(a[1]);
+    for (int i = 1; i <= n - 1; i++) {
+        ans += abs(a[i + 1] - a[i]);
     }
 
     cout << ans;
